4dbdccdf346660e7224505364d504e8b3d93fe3a
ajota0502 <ajota0502@gmail.com>
2026-05-22 21:01:24 -0600

    Add project files.

---
+++ olcEngine3d/olcEngine3d.cpp
@@ -0,0 +1,84 @@
+#include "olcConsoleGameEngine.h"
+using namespace std;
+
+struct vec3d
+{
+	float x, y, z;
+};
+
+struct triangle
+{
+	vec3d p[3];
+};
+
+struct mesh
+{
+	vector<triangle> tris;
+};
+
+class olcEngine3d : public olcConsoleGameEngine
+{
+public:
+    olcEngine3d(){
+		m_sAppName = L"3D Demo";
+    }
+private:
+	mesh meshCube;
+
+public:
+    bool OnUserCreate() override {
+		meshCube.tris = {
+			// SOUTH
+			{ 0.0f, 0.0f, 0.0f,   0.0f, 1.0f, 0.0f,   1.0f, 1.0f, 0.0f },
+			{ 0.0f, 0.0f, 0.0f,   1.0f, 1.0f, 0.0f,   1.0f, 0.0f, 0.0f },
+			// EAST
+			{ 1.0f, 0.0f, 0.0f,   1.0f, 1.0f, 0.0f,   1.0f, 1.0f, 1.0f },
+			{ 1.0f, 0.0f, 0.0f,   1.0f, 1.0f, 1.0f,   1.0f, 0.0f, 1.0f },
+			// WEST
+			{ 0.0f, 0.0f, 0.0f,   0.0f, 1.0f, 1.0f,   0.0f, 1.0f, 0.0f },
+			{ 0.0f, 0.0f, 0.0f,   0.0f, 0.0f, 1.0f,   0.0f, 1.0f, 1.0f },
+			// NORTH
+			{ 0.0f, 0.0f, 1.0f,   0.0f, 1.0f, 1.0f,   1.0f, 1.0f, 1.0f },
+			{ 0.0f, 0.0f, 1.0f,   1.0f, 1.0f, 1.0f,   1.0f, 0.0f, 1.0f },
+			// TOP
+			{ 0.0f, 1.0f, 0.0f,   0.0f, 1.0f, 1.0f,   1.0f, 1.0f, 1.0f },
+			{ 0.0f, 1.0f, 0.0f,   1.0f, 1.0f, 1.0f,   1.0f, 1.0f, 0.0f },
+			// BOTTOM
+			{ 0.0f, 0.0f, 0.0f,   1.0f, 0.0f, 1.0f,   0.0f, 0.0f, 1.0f },
+			{ 0.0f, 0.0f, 0.0f,   1.0f, 0.0f, 0.0f,   1.0f, 0.0f, 1.0f },
+		
+		};
+		
+		return true;
+    }
+
+	bool OnUserUpdate(float fElapsedTime) override {
+		Fill(0, 0, m_nScreenWidth, m_nScreenHeight, L' ');
+
+		// Draw Triangles
+		for (auto tri : meshCube.tris) {
+
+		}
+        return true;
+    }
+};
+
+using namespace std;
+int main()
+{
+	olcEngine3d demo;
+	if (demo.ConstructConsole(256, 240, 4, 4))
+		demo.Start();
+    return 0;
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
